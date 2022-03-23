d4be2e5703ac6bc181d286f46b01a07ab21b8e1c
Léo Llorca <leo.llorca@hotmail.com>
2022-03-23 01:28:42 +0100

    First user story completed

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log("Welcome on Tasks tracker codebase !");
+const createButton = document.querySelector("#createTaskButton");
+const createInput = document.querySelector("#taskTitleInput");
+createButton.setAttribute("disabled", "");
+
+createInput.addEventListener("keyup", updateButtonDisability);
+
+function updateButtonDisability() {
+  const inputValue = createInput.value;
+  if (inputValue) {
+    createButton.removeAttribute("disabled");
+  } else {
+    createButton.setAttribute("disabled", "");
+  }
+}
+
+createButton.addEventListener("click", create);
+
+function create() {
+  const inputValue = createInput.value;
+  const tasksList = document.querySelector("#tasksList");
+  const task = {
+    title: inputValue,
+    date: new Date(),
+  };
+  console.log(task);
+}
