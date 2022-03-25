f37fd0a4eb6fc10463297771fe07f25f9f2ee4bf
Léo Llorca <leo.llorca@hotmail.com>
2022-03-26 00:51:26 +0100

    Third user story completed

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const tasksList = document.querySelector("#tasksList");
 createButton.setAttribute("disabled", "");
 
 createInput.addEventListener("keyup", updateButtonDisability);
+createButton.addEventListener("click", createTask);
 
 function updateButtonDisability() {
   const inputValue = createInput.value;
@@ -13,8 +14,6 @@ function updateButtonDisability() {
     createButton.setAttribute("disabled", "");
   }
 }
-
-createButton.addEventListener("click", createTask);
 
 const tasksArray = [];
 
@@ -28,16 +27,28 @@ function createTask() {
   renderTasks();
 }
 
+function deleteTask(index) {
+  tasksArray.splice(index, 1);
+}
+
 function renderTasks() {
   tasksList.innerHTML = null;
-  tasksArray.forEach(function (task) {
+  tasksArray.forEach(function (task, index) {
     const taskBox = document.createElement("div");
     const taskTitle = document.createElement("h2");
     const taskDate = document.createElement("h3");
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("btn", "btn-danger");
     taskTitle.innerHTML = task.title;
     taskDate.innerHTML = task.date;
+    deleteButton.innerHTML = `Delete`;
     tasksList.appendChild(taskBox);
     taskBox.appendChild(taskTitle);
     taskBox.appendChild(taskDate);
+    taskBox.appendChild(deleteButton);
+    deleteButton.addEventListener("click", function () {
+      deleteTask(index);
+      renderTasks();
+    });
   });
 }
