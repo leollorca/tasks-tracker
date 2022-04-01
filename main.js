3916504bae888acc48fbf49c5592939624b1a5b5
Léo Llorca <leo.llorca@hotmail.com>
2022-04-02 00:03:53 +0200

    Fourth user story completed

--- main.js
+++ main.js
@@ -33,6 +33,7 @@ function createTask() {
   const task = {
     title: inputValue,
     date: new Date(),
+    state: "undone",
   };
   tasksArray.push(task);
   createInput.value = "";
@@ -43,23 +44,38 @@ function deleteTask(index) {
   tasksArray.splice(index, 1);
 }
 
+function doneTask(task) {
+  task.state = "done";
+}
+
 function renderTasks() {
   tasksList.innerHTML = null;
   tasksArray.forEach(function (task, index) {
     const taskBox = document.createElement("div");
     const taskTitle = document.createElement("h2");
+    if (task.state === "done") {
+      taskTitle.style.textDecoration = "line-through";
+    }
     const taskDate = document.createElement("h3");
     const deleteButton = document.createElement("button");
+    const doneButton = document.createElement("button");
     deleteButton.classList.add("btn", "btn-danger");
+    doneButton.classList.add("btn", "btn-primary");
     taskTitle.innerHTML = task.title;
     taskDate.innerHTML = task.date;
     deleteButton.innerHTML = `Delete`;
+    doneButton.innerHTML = `Done`;
     tasksList.appendChild(taskBox);
     taskBox.appendChild(taskTitle);
     taskBox.appendChild(taskDate);
     taskBox.appendChild(deleteButton);
+    taskBox.appendChild(doneButton);
     deleteButton.addEventListener("click", function () {
       deleteTask(index);
+      renderTasks();
+    });
+    doneButton.addEventListener("click", function () {
+      doneTask(task);
       renderTasks();
     });
   });
